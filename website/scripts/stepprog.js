d88d9c72a799835a4fc3f13c819f722546c778e2
John Tran <johntradiction@gmail.com>
2018-02-27 19:26:20 -0800

    binary array

--- website/scripts/stepprog.js
+++ website/scripts/stepprog.js
@@ -1,4 +1,3 @@
-let count;
 let start=prompt('Enter the start:');
 let startInt=parseInt(start);
 let end=prompt('Enter the end:');
@@ -6,20 +5,26 @@ let endInt=parseInt(end);
 let step=prompt('Enter the step:');
 let stepInt=parseInt(step);
 var stepArray= new Array();
-
-if (isNaN(startInt)||isNaN(endInt)||isNaN(stepInt)||stepInt==0) {
-    document.write('Invalid integer input, please reload and try again.')
+var total=0;
+var binArray= new Array();
+if (isNaN(startInt)||isNaN(endInt)||isNaN(stepInt)||stepInt==0||startInt<endInt && stepInt<0||startInt>endInt && stepInt>0) {
+    document.write('Invalid integer input, please reload and try again.');
 }
 else {
     if (step>0) {
-        for (count=startInt; count<=endInt; count+=stepInt){
-            stepArray.push(count);
+        for (startInt; startInt<=endInt; startInt+=stepInt) {
+            stepArray.push(startInt);
         }
     }
     if (step<0) {
-        for (count=startInt; count>=endInt; count+=stepInt){
-            stepArray.push(count);
+        for (startInt; startInt>=endInt; startInt+=stepInt) {
+            stepArray.push(startInt);
         }
     }
-    document.write('The generated array is ' + stepArray)
+    for (var i=0, len=stepArray.length; i<len; i++) {
+        total+=stepArray[i]; binArray.push(stepArray[i].toString(2))
+    }
+    document.write('The generated array is ' + stepArray + '.' + '<br>');
+    document.write('The sum is ' + total + '.' + '<br>')
+    document.write('The binary of absolute element values are: ' + binArray + '.')
 }
