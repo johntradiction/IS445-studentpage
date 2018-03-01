cd927c629032f1403d4d702562966cdf77228cf5
John Tran <johntradiction@gmail.com>
2018-02-28 19:52:05 -0800

    change == to ===

--- website/scripts/stepprog.js
+++ website/scripts/stepprog.js
@@ -4,7 +4,7 @@ let step=parseInt(prompt('Enter the step:'));
 let stepArray= new Array();
 let total=0;
 let binArray= new Array();
-if (isNaN(start)||isNaN(end)||isNaN(step)||step==0||start<end && step<0||start>end && step>0) {
+if (isNaN(start)||isNaN(end)||isNaN(step)||step===0||start<end && step<0||start>end && step>0) {
     document.write('Invalid integer input, please reload and try again.');
 }
 else {
@@ -19,9 +19,9 @@ else {
         }
     }
     for (var i=0, len=stepArray.length; i<len; i++) {
-        total+=stepArray[i]; binArray.push(stepArray[i].toString(2))
+        total+=stepArray[i]; binArray.push(stepArray[i].toString(2));
     }
     document.write('The generated array is ' + stepArray + '.' + '<br>');
-    document.write('The sum is ' + total + '.' + '<br>')
-    document.write('The binary of absolute element values are: ' + binArray + '.')
+    document.write('The sum is ' + total + '.' + '<br>');
+    document.write('The binary of absolute element values are: ' + binArray + '.');
 }
